ac70bde6a4f10a6096699b2974c92eae3c455caf
Miles Peyton <milespeyton@gmail.com>
2014-07-24 23:20:52 -0400

    Add seth.d.

---
+++ seth.d
@@ -0,0 +1,22 @@
+sethd re I-se .bd, 
+
+Eth.mim, I-se. 
+
+Se-b lb’I, Se-c’cr
+
+> hees:  ee er E.R. Lse ;; : mimne.r ;; : ensys.f
+> sethd: Er SoUlii uFur.l >> ‘er’
+Sot,, Wt Wn OR]mer.l
+
+sethd re M-B .bd,
+
+.55 mXi beth  ; .
+.62 mPi beth  ; /
+
+: . : . : rrl irrly rrls : . : . :
+rrl btk mim-sec c .bd’s 
+  : . : . : ______: . : . :
+
+on.üs globro,
+u,su gal EC.
+
